631644d11308b2fe028262e649bc6fbf76fc08f0
salouma11 <islembenamar23@gmail.com>
2023-11-25 18:56:32 +0100

    first hi

---
+++ TestGit.Classes/Personne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit.Classes
+{
+    public class Personne
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adresse { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
